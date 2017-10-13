2e4689079015422feaa601dc1ebe7b68f599949a
nimbyxcialist <32730976+nimbyxcialist@users.noreply.github.com>
2017-10-13 15:34:05 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 
 // Env vars should be casted to correct types
 const config = {
-  PORT: Number(process.env.PORT) || 9000,
+  PORT: Number(process.env.PORT) || 80,
   NODE_ENV: process.env.NODE_ENV,
   LOG_LEVEL: process.env.LOG_LEVEL,
   ALLOW_HTTP: process.env.ALLOW_HTTP === 'true',
